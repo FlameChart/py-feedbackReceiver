fb90e199e1e0534d8d344782186f3b65412f2317
welki/FlameC <91359913+FlameChart@users.noreply.github.com>
2025-02-26 15:00:56 +0800

    fix: 修复了开发/生产环境的配置不走 env 文件的问题

--- run_dev.py
+++ run_dev.py
@@ -1,8 +1,13 @@
 from app import app
+from dotenv import load_dotenv
+import os
+
+# 加载开发环境配置
+load_dotenv('.env.dev')
 
 if __name__ == '__main__':
     app.run(
-        host='127.0.0.1',
-        port=5000,
-        debug=True
+        host=os.getenv('HOST', '127.0.0.1'),
+        port=int(os.getenv('PORT', 8220)),
+        debug=bool(int(os.getenv('FLASK_DEBUG', 1)))
     ) 
--- run_prod.py
+++ run_prod.py
@@ -1,8 +1,13 @@
 from app import app
+from dotenv import load_dotenv
+import os
+
+# 加载生产环境配置
+load_dotenv('.env.prod')
 
 if __name__ == '__main__':
     app.run(
-        host='0.0.0.0',
-        port=8000,
-        debug=False
+        host=os.getenv('HOST', '0.0.0.0'),
+        port=int(os.getenv('PORT', 8000)),
+        debug=bool(int(os.getenv('FLASK_DEBUG', 0)))
     ) 
