9f012d3aa7f55b9887e8432062d86405b44834c1
welki/FlameC <91359913+FlameChart@users.noreply.github.com>
2025-03-02 09:58:22 +0800

    feat: 增加了 SSL 支持（需在 .env.prod 中配置）

--- run_prod.py
+++ run_prod.py
@@ -6,8 +6,17 @@ import os
 load_dotenv('.env.prod')
 
 if __name__ == '__main__':
-    app.run(
-        host=os.getenv('HOST', '0.0.0.0'),
-        port=int(os.getenv('PORT', 8000)),
-        debug=bool(int(os.getenv('FLASK_DEBUG', 0)))
-    ) 
+    if os.getenv('SSL_ENABLED', '0') == '1':
+        app.run(
+            host=os.getenv('HOST', '0.0.0.0'),
+            port=int(os.getenv('PORT', 443)),
+            debug=bool(int(os.getenv('FLASK_DEBUG', 0))),
+            ssl_context=(os.getenv('SSL_CERT_PATH'), os.getenv('SSL_KEY_PATH'))
+        )
+        exit()
+    else:
+        app.run(
+            host=os.getenv('HOST', '0.0.0.0'),
+            port=int(os.getenv('PORT', 8000)),
+            debug=bool(int(os.getenv('FLASK_DEBUG', 0)))
+        )
